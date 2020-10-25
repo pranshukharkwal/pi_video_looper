27b3c85e083cada842381feb98b7e6dfbdb44f52
= <awesomepranshu@gmail.com>
2020-10-26 02:36:31 +0530

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name              = 'Adafruit_Video_Looper',
       description       = 'Application to turn your Raspberry Pi into a dedicated looping video playback device, good for art installations, information displays, or just playing cat videos all day.',
       license           = 'GNU GPLv2',
       url               = 'https://github.com/adafruit/pi_video_looper',
-      install_requires  = ['pyudev', 'pygame'],
+      install_requires  = ['pyudev', 'pygame', 'opencv-python' , 'moviepy'],
       packages          = find_packages())
